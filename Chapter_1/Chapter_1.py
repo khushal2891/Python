f454e39f13c43b64b34d5a3920744c49853734e2
khushal2891 <khushalsharma2891@gmail.com>
2023-01-22 21:56:47 +0530

    Chapter_1_22/jan/2023

---
+++ Chapter_1/Chapter_1.py
@@ -0,0 +1,6 @@
+print("hello world")
+print('''twinkle twinkle little star''')
+from playsound import playsound
+playsound ('C:\\Users\\acer\\OneDrive\\Documents\\All documents\\SGSITS clg\\play.mp3')
+
+# chapter 1 END
