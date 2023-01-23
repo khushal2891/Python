ccb815c194fb5fb7cfae69566d19e7067259e595
khushal2891 <khushalsharma2891@gmail.com>
2023-01-23 22:48:05 +0530

    Chapter_3_done

---
+++ Chapter_3/chapter_3_string.py
@@ -0,0 +1,11 @@
+# Quesstion-2
+
+# a=input("Enter name : ")
+# b=input("Enter date : ")
+# b=int(b)
+# print('''Dear''',a,
+#         'You are selected on date',b ,'!')
+
+# # Question-2
+# a="once upon a time in mumbai"
+# print(a.replace(" ","   spacee    "))
