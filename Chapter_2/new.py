781ecd35e8be7eb9fda5f4cd3b5c5caa8fdba5ae
khushal2891 <khushalsharma2891@gmail.com>
2023-01-23 20:51:42 +0530

    Chater_2_done

---
+++ Chapter_2/new.py
@@ -0,0 +1,20 @@
+# input of a number 
+
+a = input("enter a number ")
+b= input ("enter a number ")
+a=int(a)
+b=int(b)
+print ("sum is :" ,a+b)
+print(type(a))
+
+# comparision operator 
+a=34
+b=80
+print(a>b)
+print(a<b)
+
+
+# Square of a number 
+a= input("enter a number :")
+a=int(a)
+print("Square of  a number is :" ,a*a)
